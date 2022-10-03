a089927024b8de21b4387173a6a7350f332e6f38
lettycodes <sps.leticia@gmail.com>
2022-10-03 16:45:17 -0300

    atualização da pasta back-end com arquivos dos exercicios do dia 03/10

--- Bootcamp Generation/Blog Pessoal/Back-end/src/main/java/com/BlogPessoal/BlogPessoal/repository/PostagemRespository.java
+++ Bootcamp Generation/Blog Pessoal/Back-end/src/main/java/com/BlogPessoal/BlogPessoal/repository/PostagemRespository.java
@@ -1,6 +1,9 @@
 package com.BlogPessoal.BlogPessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.BlogPessoal.BlogPessoal.model.Postagem;
@@ -8,4 +11,5 @@ import com.BlogPessoal.BlogPessoal.model.Postagem;
 @Repository
 public interface PostagemRespository extends JpaRepository<Postagem, Long> {
 
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
