45c246f614b3a132457c782526e5e7f2a23df258
lettycodes <sps.leticia@gmail.com>
2022-10-07 22:20:06 -0300

    adiciona banco de dados da loja de games

---
+++ Bootcamp Generation/Bloco 2/Spring Boot - Java/LojaDeGames/src/main/java/com/LojaDeGames/LojaDeGames/model/Produto.java
@@ -0,0 +1,95 @@
+package com.LojaDeGames.LojaDeGames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O nome obrigatório!")
+	@Size(min = 1, max = 100)
+	private String nome;
+
+	@NotBlank(message = "A produtora é obrigatória!")
+	@Size(min = 1, max = 100)
+	private String produtora;
+
+	@NotBlank(message = "A modalidade é obrigatória!")
+	private String modalidade;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull
+	@Positive(message = "O preço deve ser maior que zero!")
+	private BigDecimal preco;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getProdutora() {
+		return produtora;
+	}
+
+	public void setProdutora(String produtora) {
+		this.produtora = produtora;
+	}
+
+	public String getModalidade() {
+		return modalidade;
+	}
+
+	public void setModalidade(String modalidade) {
+		this.modalidade = modalidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
